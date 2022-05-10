a29183a427c3453d35e7d4cd79594cbba1badf27
Mateus Matos <mateus.m.oliveira2303@gmail.com>
2022-05-10 20:57:03 -0300

    Finalizado validação de senha

--- Layout/javascript/script.js
+++ Layout/javascript/script.js
@@ -36,13 +36,39 @@ email.addEventListener('keyup', ()=>{
     if(minusculo.indexOf('@') == -1 || minusculo.indexOf(".com") == -1){
         //console.log("Email inválido")
         msgEmail.textContent = "Este email é inválido"
-        msgEmail.classList.remove("text-success",)
+        msgEmail.classList.remove("text-success")
         msgEmail.classList.add("text-danger")
     }
     else{
         //console.log("Email válido")
         msgEmail.textContent = "Este email é válido"
-        msgEmail.classList.remove("text-danger",)
+        msgEmail.classList.remove("text-danger")
         msgEmail.classList.add("text-success")
     }
 })
+
+/* HABILITAR A VISUALIZAÇÃO DA SENHA */
+
+let senha = document.querySelector("#senha")
+let iconEye = document.querySelector("#senha + span")// ESTOU PEGANDO A TAG SPAN EXATAMENTE APOS A TAG QUE POSSUI UM ID = "SENHA"
+iconEye.addEventListener('click',()=>{
+    if(iconEye.textContent == "visibility"){
+        iconEye.textContent = "visibility_off"
+        senha.setAttribute("type", "password")
+    }
+    else{
+        iconEye.textContent = "visibility"
+        senha.setAttribute("type", "text")
+    }
+})
+
+/* PEGANDO INFORMAÇÕES DO RADIO */
+
+let escolaridade = document.querySelectorAll("input[name=escolaridade]")
+let btnEscolaridade = document.querySelector("#btnEscolaridade")
+
+btnEscolaridade.addEventListener("click",()=>{
+    console.log(escolaridade)
+})
+   
+    
